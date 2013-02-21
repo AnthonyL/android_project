2793c8d57cbfa30320af48dbd1f37e0768fe0e95
schapeyroux <chapeyroux.sebastien@gmail.com>
2013-02-21 11:17:48 +0100

    creation vue menu principal

--- src/ter/android_project/MainActivity.java
+++ src/ter/android_project/MainActivity.java
@@ -3,13 +3,25 @@ package ter.android_project;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	private TextView titreApp, legendApp;
+	private Button bPlay, bHighScore, bHelp, bExit;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+			
+
+		bPlay = (Button) findViewById(R.id.buttonPlay);
+		bHighScore = (Button) findViewById(R.id.buttonHighScore);
+		bHelp = (Button) findViewById(R.id.buttonHelp);
+		bExit = (Button) findViewById(R.id.buttonExit);
+		//titreApp = (TextView) findViewById
 	}
 
 	@Override
